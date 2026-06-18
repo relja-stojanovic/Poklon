f448e214e2eea871643c1664a12ac187bc8235a0
relja-stojanovic <reljastojanovic2007@gmail.com>
2026-06-18 12:26:38 +0200

    export

--- file/user_settings/UserSettings.gd
+++ file/user_settings/UserSettings.gd
@@ -1,7 +1,7 @@
 class_name UserSettings extends Resource
 
 @export var sfx: Setting = Setting.new()
-@export var music: Setting = Setting.new()
-@export var preview: Setting = Setting.new()
+@export var music: Setting = Setting.new(true)
+@export var preview: Setting = Setting.new(true)
 @export var window_chat: Setting = Setting.new()
 @export_storage var window_chat_size: Vector2 = Vector2(200, 300)
--- icon/script.gd
+++ icon/script.gd
@@ -1,11 +1,13 @@
 extends Control
 
+@export var filename: String = "icon128x128"
+
 func _ready() -> void:
 	await RenderingServer.frame_post_draw
 	var viewport = $SubViewportContainer/SubViewport
 	var image = viewport.get_texture().get_image()
 
-	var error = image.save_png("res://assets/texture/icon.png")
+	var error = image.save_png("res://assets/texture/%s.png"%filename)
 	
 	if error == OK:
 		print("Icon saved successfully!")
